012706a57ea16226304431fe4746f33fc341d700
Abhishek Bansal <abhishekbansal2312@gmail.com>
2025-04-21 02:36:29 +0530

    Refactor routing in App component and add new pages for Course, Coaching, Login, and Signup

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Footer from "./components/footer/Footer";
 import AboutUs from "./pages/AboutUs";
+import Course from "./pages/Course";
+import Coaching from "./pages/Coaching";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 export default function App() {
   return (
@@ -13,6 +17,10 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutUs />} />
+          <Route path="/course" element={<Course />} />
+          <Route path="/coaching" element={<Coaching />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
         </Routes>
 
         <Footer />
