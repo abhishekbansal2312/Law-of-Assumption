3e9aade986f43de8a2d26144a880a9a13c359a08
Abhishek Bansal <abhishekbansal2312@gmail.com>
2025-04-21 02:37:38 +0530

    Refactor HeroSection component in HomePage to add image, title, and description props

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,19 @@
-import HeroSection from "../components/homepage/HeroSection";
+import HeroSection from "../components/common/HeroSection";
 import AboutUsSection from "../components/homepage/AboutUs";
 import WhatWeOfferSection from "../components/homepage/OfferSection";
 import ManifestationCategories from "../components/homepage/ManifestationCategories";
 import WhyTrustUsSection from "../components/homepage/WhyTrustUsSection";
 import TestimonialsSection from "../components/homepage/TestimonialsSection";
+import banner from "../assets/banner1.png";
 
 export default function HomePage() {
   return (
     <div>
-      <HeroSection />
+      <HeroSection
+        image={banner}
+        title={"Manifest Your Dream Life with the Law of Assumption"}
+        description={"Create Your Reality with the Law of Assumption"}
+      />
       <AboutUsSection />
       <WhatWeOfferSection />
       <ManifestationCategories />
